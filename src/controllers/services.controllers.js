47f45d6c17ca5a4ceda892a1f72d25b28a3e4dac
Renata Inacio <revazgauska@gmail.com>
2023-08-14 15:37:16 -0300

    chore: adjust code for deploy on render

--- src/controllers/services.controllers.js
+++ src/controllers/services.controllers.js
@@ -30,7 +30,6 @@ export async function getServices(req, res){
 export async function postService(req, res){
     let service = req.body;
     service = {...service, userId: res.locals.user.id};
-    console.log(service);
     try {
         await insertService(service);
         return res.sendStatus(201);
@@ -56,7 +55,6 @@ export async function deleteService(req, res){
     const { id } = req.params;
     try {
         const service = await getServiceById(id);
-        console.log(service.rows);
         if (service.rows.length === 0) return res.sendStatus(404);
         if (service.rows[0].userId !== res.locals.user.id) return res.sendStatus(401);
         await delService(id);
